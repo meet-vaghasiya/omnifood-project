51aea64b8140a14d1ce545d729254a32bc34ad79
meet-vaghasiya <meetvaghasiya2012@gmail.com>
2021-08-31 11:33:10 +0530

    gap propery issue in safari is fix

--- js/main.js
+++ js/main.js
@@ -35,7 +35,64 @@ const obs = new IntersectionObserver(function (entires) {
 obs.observe(sectionHeroEl)
 
 
+// for smooth scrolling
+
+const allLinks = document.querySelectorAll('a:link')
+
+allLinks.forEach(function (link) {
+    link.addEventListener('click', function (el) {
+        el.preventDefault()
+        const href = link.getAttribute('href')
+        console.log(href)
+        if (href === '#') {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            })
+        }
+        //scroll to other links
+        if (href !== '#' && href.startsWith('#')) {
+
+            const sectionEl = document.querySelector(href)
+            sectionEl.scrollIntoView({
+                behavior: 'smooth'
+            })
+        }
+
+        // close mobile views
+        if (link.classList.contains('main-nav-link')) {
+            headerEl.classList.toggle('nav-open')
+        }
+
+    })
+
+})
 
 
- // event listeneres
+
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+// paste below code for adding class in body
+
+function checkFlexGap() {
+    var flex = document.createElement("div");
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
+
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported);
+
+    if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+
+
 
