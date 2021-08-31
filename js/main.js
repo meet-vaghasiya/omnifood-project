385a90884aaa04f1f4603b83b325146c3673832b
meet-vaghasiya <meetvaghasiya2012@gmail.com>
2021-08-31 10:04:54 +0530

    sticky navigation part complete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const yearEl = document.querySelector('.year')
 const btnNavEl = document.querySelector('.btn-mobile-nav')
 const headerEl = document.querySelector('.header')
+const body = document.querySelector('body')
+
+
 
 // set current year
 yearEl.textContent = new Date().getFullYear()
@@ -9,6 +12,27 @@ yearEl.textContent = new Date().getFullYear()
 btnNavEl.addEventListener('click', function () {
     headerEl.classList.toggle('nav-open')
 })
+
+// stickey navigation
+
+const sectionHeroEl = document.querySelector('.section-hero')
+const obs = new IntersectionObserver(function (entires) {
+    const ent = entires[0]
+
+    if (ent.isIntersecting === false) {
+        body.classList.add('stickey')
+    } else {
+        body.classList.remove('stickey')
+
+    }
+
+}, {
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px'
+})
+
+obs.observe(sectionHeroEl)
 
 
 
